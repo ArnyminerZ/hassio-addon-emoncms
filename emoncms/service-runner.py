cf9161a979fe14fd0b69cf588e7a4d42f46e739f
Arnau Mora <arnyminer.z@gmail.com>
2022-01-07 16:23:37 +0100

    Added disabled redis warning

--- emoncms/service-runner.py
+++ emoncms/service-runner.py
@@ -11,6 +11,8 @@ import subprocess
 import time
 import shlex
 import redis
+import os
+import logging
 
 KEYS = ["service-runner", "emoncms:service-runner"]
 
@@ -18,17 +20,24 @@ KEYS = ["service-runner", "emoncms:service-runner"]
 def connect_redis():
     while True:
         try:
-            server = redis.Redis()
+            server = redis.Redis(
+                host=os.getenv('REDIS_HOST'),
+                port=os.getenv('REDIS_PORT'),
+            )
             if server.ping():
-                print("Connected to redis server", flush=True)
+                logging.info("Connected to redis server", flush=True)
                 return server
         except redis.exceptions.ConnectionError:
-            print("Unable to connect to redis server, sleeping for 30s", flush=True)
+            logging.error("Unable to connect to redis server, sleeping for 30s", flush=True)
         time.sleep(30)
 
 
 def main():
-    print("Starting service-runner", flush=True)
+    if not os.getenv('REDIS_ENABLED'):
+        logging.warn("Redis is disabled. Won't start runner.")
+        return
+
+    logging.info("Starting service-runner", flush=True)
     server = connect_redis()
     while True:
         try:
@@ -38,14 +47,14 @@ def main():
                 continue
             flag = packed[1].decode()
         except redis.exceptions.ConnectionError:
-            print("Connection to redis server lost, attempting to reconnect", flush=True)
+            logging.error("Connection to redis server lost, attempting to reconnect", flush=True)
             server = connect_redis()
             continue
 
-        print("Got flag:", flag, flush=True)
+        logging.info("Got flag:", flag, flush=True)
         if ">" in flag:
             script, logfile = flag.split(">")
-            print("STARTING:", script, '&>', logfile, flush=True)
+            logging.info("STARTING:", script, '&>', logfile, flush=True)
             # Got a cmdline, now run it.
             with open(logfile, "w") as f:
                 try:
@@ -58,13 +67,13 @@ def main():
                     continue
         else:
             script = flag
-            print("STARTING:", script, flush=True)
+            logging.info("STARTING:", script, flush=True)
             try:
                 subprocess.call(shlex.split(script), stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
             except Exception as exc:
                 continue
 
-        print("COMPLETE:", script, flush=True)
+        logging.info("COMPLETE:", script, flush=True)
 
 
 if __name__ == "__main__":
